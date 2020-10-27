06d01730c5065f5247978308515a008077b293c8
Nicolas Lepore <nlepore33@gmail.com>
2020-10-27 14:03:01 -0400

    auto

--- gym_bertrandcompetition/__init__.py
+++ gym_bertrandcompetition/__init__.py
@@ -4,10 +4,10 @@ from gym.envs.registration import register
 logger = logging.getLogger(__name__)
 
 register(
-    id='BertrandCompetitionDiscrete',
+    id='BertrandCompetitionDiscrete-v0',
     entry_point='gym_bertrandcompetition.envs:BertrandCompetitionDiscreteEnv',
 )
 register(
-    id='BertrandCompetitionContinuous',
+    id='BertrandCompetitionContinuous-v0',
     entry_point='gym_bertrandcompetition.envs:BertrandCompetitionContinuousEnv',
 )
