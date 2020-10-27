39dad1aa19a2f6a636055b45c485ee91f4f380d6
Nicolas Lepore <31548696+nlepore33@users.noreply.github.com>
2020-10-27 13:00:07 -0400

    Update __init__.py

--- gym_bertrandcompetition/__init__.py
+++ gym_bertrandcompetition/__init__.py
@@ -1,10 +1,10 @@
 from gym.envs.registration import register
 
 register(
-    id='foo-v0',
-    entry_point='gym_foo.envs:FooEnv',
+    id='BertrandCompetitionMarketDiscrete',
+    entry_point='gym_foo.envs:BertrandCompetitionMarketDiscreteEnv',
 )
 register(
-    id='foo-extrahard-v0',
-    entry_point='gym_foo.envs:FooExtraHardEnv',
+    id='BertrandCompetitionMarketContinuous',
+    entry_point='gym_foo.envs:BertrandCompetitionMarketContinuousEnv',
 )
