64800ce758a065afb5f51a8e49f9a41d069d8760
Nicolas Lepore <nlepore33@gmail.com>
2020-10-27 14:21:49 -0400

    auto

--- gym_bertrandcompetition/envs/bertrand_competition_discrete.py
+++ gym_bertrandcompetition/envs/bertrand_competition_discrete.py
@@ -11,11 +11,39 @@ class BetrandCompetitionDiscreteEnv(Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, n = 2, c_i = 1, a_0 = 0, mu = 0.25, delta = 0.95, m = 15, xi = 0.1, k = 1, pN = 1, pM = 10):
+
         self.action_space = np.linspace(pN - xi * (pM - pN), pM + xi * (pM - pN), m)
-        self.state = FixedList(n = k)
+        self.observation_space = FixedList(n = k)
+        self.reward_range = (-float('inf'), float('inf'))
+        
 
     def demand(a, p, mu):
         q = np.exp((a - p) / mu) / (np.sum(np.exp((a - p) / mu)) + np.exp(a[0] / mu))
         return q
+
+    def step(self, action_n):
+        obs_n = []
+        reward_n = []
+        done_n = []
+        info_n = {'n': []}
+
+        return obs_n, reward_n, done_n, info_n
+
+    # get reward for a particular agent
+    def _get_reward(self, agent):
+        if self.reward_callback is None:
+            return 0.0
+        return self.reward_callback(agent, self.world)
+
+    # set env action for a particular agent
+    def _set_action(self, action, agent, action_space, time=None):
+        raise NotImplementedError
+
+    def reset(self):
+        raise NotImplementedError
+
+    def render(self, mode='human'):
+        raise NotImplementedError
+
 
 bcd = BetrandCompetitionDiscreteEnv()
