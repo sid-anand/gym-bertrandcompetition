2f9f48f6946bef93c22a9766e3f04fcbd4b5e139
Nicolas Lepore <nlepore33@gmail.com>
2020-10-27 20:56:04 -0400

    auto

--- gym_bertrandcompetition/envs/bertrand_competition_discrete.py
+++ gym_bertrandcompetition/envs/bertrand_competition_discrete.py
@@ -7,37 +7,50 @@ import numpy as np
 from io import StringIO
 from fixedlist import FixedList
 
+# cd OneDrive/Documents/Research/gym-bertrandcompetition/gym_bertrandcompetition/envs
+
 class BetrandCompetitionDiscreteEnv(Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, n = 2, c_i = 1, a_0 = 0, mu = 0.25, delta = 0.95, m = 15, xi = 0.1, k = 1, pN = 1, pM = 10):
         self.n = n
+        self.c_i = c_i
+        self.a_0 = a_0
         self.action_space = np.linspace(pN - xi * (pM - pN), pM + xi * (pM - pN), m)
-        self.observation_space = FixedList(n = k)
+        self.observation_space = self.action_space.copy()
         self.reward_range = (-float('inf'), float('inf'))
+        self.obs_n = FixedList(n = k)
         
 
-    def demand(self, a, p, mu):
-        q = np.exp((a - p) / mu) / (np.sum(np.exp((a - p) / mu)) + np.exp(a[0] / mu))
-        return q
+    # def demand(self, a, p, mu):
+    #     q = np.exp((a - p) / mu) / (np.sum(np.exp((a - p) / mu)) + np.exp(self.a_0 / mu))
+    #     return q
+
+    def demand(self, price):
+        return -price + 12
 
     def step(self, action_n):
-        obs_n = []
-        reward_n = [0] * self.n
-        done_n = []
+
+        action_n = np.array(action_n)
+
+        reward_n = np.array([0.0] * self.n)
+        done_n = [True] * self.n
         info_n = {'n': []}
 
-        self.observation_space.add(action_n)
+        self.obs_n.add(action_n.tolist())
 
-        min_price = np.min(actions_n)
-
-        if min_price != np.max(action_n):
-            reward_n[np.argmin(actions_n)] = self.demand(a, p, mu) * min_price
+        if np.max(action_n) > self.c_i:
+            min_price = min(action_n[action_n > self.c_i], default = self.c_i)
+            total_profit = (min_price - self.c_i) * self.demand(min_price) # self.demand(11, min_price, 0.05)
+            min_price_idxs = np.where(action_n == min_price)[0]
+            reward_n[min_price_idxs] = total_profit / min_price_idxs.size
 
         # action of first - action of second, but need actual demand function for this
         # demand is amount of spend, if the price is high then the quantity demanded is low, vice versa
 
-        return obs_n, reward_n, done_n, info_n
+        print(self.obs_n, reward_n, done_n, info_n)
+
+        return self.obs_n, reward_n, done_n, info_n
 
     # get reward for a particular agent
     def _get_reward(self, agent):
@@ -57,3 +70,9 @@ class BetrandCompetitionDiscreteEnv(Env):
 
 
 bcd = BetrandCompetitionDiscreteEnv()
+for i in range(14):
+    print()
+    bcd.step([bcd.action_space[i], bcd.action_space[i]])
+    print()
+    bcd.step([bcd.action_space[i], bcd.action_space[i+1]])
+    # bcd.step([bcd.action_space[i+1], bcd.action_space[i]])
