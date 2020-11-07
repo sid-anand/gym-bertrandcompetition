0c727aaa7a6500f754e236ffc1ce3933b8ebab44
Nicolas Lepore <nlepore33@gmail.com>
2020-11-07 11:50:34 -0500

    auto

--- gym_bertrandcompetition/envs/bertrand_competition_discrete.py
+++ gym_bertrandcompetition/envs/bertrand_competition_discrete.py
@@ -17,28 +17,36 @@ class BertrandCompetitionDiscreteEnv(MultiAgentEnv):
 
         super(BertrandCompetitionDiscreteEnv, self).__init__()
         self.num_agents = num_agents
-        self.k = k
-        self.c_i = c_i
-        self.a_0 = a_0
-        self.mu = mu
-        self.pN = c_i
-        self.current_step = None
-        self.max_steps = max_steps
-        self.players = [ 'agent_' + str(i) for i in range(num_agents)]
-        self.a = np.array([c_i + a_minus_c_i])
-        self.action_history = {}
-        for i in range(num_agents):
-            if self.players[i] not in self.action_history:
-                self.action_history[self.players[i]] = [0] * k
 
+        # Length of Memory
+        self.k = k
+
+        # Marginal Cost
+        self.c_i = c_i
+
+        # Product Quality Indexes
+        self.a = np.array([c_i + a_minus_c_i])
+
+        # Product Quality Index: Outside Good
+        self.a_0 = a_0
+
+        # Index of Horizontal Differentiation
+        self.mu = mu
+
+        # Nash Equilibrium Price
+        self.pN = c_i
+
+        # Monopoly Equilibrium Price
         monopoly_profit = []
         price_range = np.arange(0, 100, 0.1)
         for i in price_range:
             monopoly_profit.append((i - c_i) * self.demand(self.a, i, self.mu))
         self.pM = price_range[np.argmax(monopoly_profit)]
 
+        # MultiAgentEnv Action Space
         self.action_space = Discrete(m)
         
+        # MultiAgentEnv Observation Space
         if k > 0:
             self.numeric_low = np.array([0] * (k * num_agents))
             numeric_high = np.array([m] * (k * num_agents))
@@ -49,15 +57,23 @@ class BertrandCompetitionDiscreteEnv(MultiAgentEnv):
             self.observation_space = Box(self.numeric_low, numeric_high, dtype=int)
 
         self.action_price_space = np.linspace(self.pN - xi * (self.pM - self.pN), self.pM + xi * (self.pM - self.pN), m)
-
         self.reward_range = (-float('inf'), float('inf'))
+        self.current_step = None
+        self.max_steps = max_steps
+        self.players = [ 'agent_' + str(i) for i in range(num_agents)]
+        self.action_history = {}
+        for i in range(num_agents):
+            if self.players[i] not in self.action_history:
+                self.action_history[self.players[i]] = [0] * k
         self.reset()
 
     def demand(self, a, p, mu):
+        ''' Demand as a function of product quality indexes, price, and mu. '''
         q = np.exp((a - p) / mu) / (np.sum(np.exp((a - p) / mu)) + np.exp(self.a_0 / mu))
         return q
 
     def step(self, actions_dict):
+        ''' MultiAgentEnv Step'''
 
         actions_idx = np.array(list(actions_dict.values())).flatten()
 
@@ -76,14 +92,14 @@ class BertrandCompetitionDiscreteEnv(MultiAgentEnv):
 
         if np.max(actions) > self.c_i:
             min_price = min(actions[actions > self.c_i], default = self.c_i)
-            total_profit = (min_price - self.c_i) * self.demand(self.a, min_price, self.mu) # self.demand(min_price)
+            total_profit = (min_price - self.c_i) * self.demand(self.a, min_price, self.mu)
             min_price_idxs = np.where(actions == min_price)[0]
             reward[min_price_idxs] = total_profit / min_price_idxs.size
 
         reward = dict(zip(self.players, reward))
         done = {'__all__': self.current_step == self.max_steps}
         info = dict(zip(self.players, [{}]*self.num_agents))
-        
+
         # print('Obs:', self.obs_n)
         # print('Reward:', reward)
         # print('Done:', done)
@@ -107,6 +123,7 @@ class BertrandCompetitionDiscreteEnv(MultiAgentEnv):
 
 
 
+# Tests
 
 # bcd = BertrandCompetitionDiscreteEnv()
 # obs = bcd.reset()
